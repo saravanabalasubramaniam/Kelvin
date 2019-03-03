410a52d956626decaa77895e039c8d2ae64ffec3
saravana <saravanasaru14313@gmail.com>
2019-03-03 06:46:55 -0800

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Kelvin
+{
+public static void main(String args[])
+  {
+	Scanner input=new Scanner(System.in);
+	int celsius=input.nextInt();
+	int kelvin=celsius+273;
+	System.out.println(kelvin);
+   }
+}	
